08363266cd0da52f99111bd3fc9c6e5e7674cac8
Valarie <Valarierono97@gmail.com>
2023-09-14 15:49:37 +0300

    Horizontal pager done

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -5,18 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,12 +54,14 @@ fun MainContent() {
     Column(
         modifier = Modifier
             .background(Color.Black)
+
     ) {
         Image(
             painter = painterResource(id = R.drawable.img),
             contentDescription ="",
             contentScale = ContentScale.FillWidth,
             modifier = Modifier
+                .height(520.dp)
         )
         MainContentInfo()
     }
@@ -60,12 +71,12 @@ fun MainContent() {
 fun MainContentInfo(){
    Column(
        modifier = Modifier
-           .padding(horizontal = 24.dp)
+           .padding(horizontal = 38.dp, vertical = 20.dp)
    ) {
        Text(
            text="Get Started",
            fontSize = 20.sp,
-           color=Color.Yellow
+           color=Color( 0xFFffa500)
        )
        Spacer(modifier = Modifier.height(10.dp))
        Text(
@@ -79,5 +90,45 @@ fun MainContentInfo(){
            fontSize = 26.sp,
            color=Color.White
        )
+       Spacer(modifier = Modifier.height(10.dp))
+       Row {
+           Button(
+               onClick = {  },
+               shape=RoundedCornerShape(10.dp),
+               colors = ButtonDefaults.buttonColors(Color.Black),
+               modifier = Modifier
+
+
+           ) {
+               Text(
+                   text ="Skip Now",
+                   fontSize = 14.sp,
+                   color= Color.White
+
+               )
+           }
+      Row (
+          horizontalArrangement = Arrangement.End,
+          modifier = Modifier
+              .padding(start=100.dp)
+      ){
+          Button(
+              onClick = {  },
+              shape=RoundedCornerShape(17.dp),
+              colors = ButtonDefaults.buttonColors(Color(0xFFffa500)),
+              modifier = Modifier
+                  .width(110.dp)
+                  .height(50.dp)
+
+
+          ) {
+              Text(
+                  text ="Next",
+
+                  )
+          }
+      }
+       }
+
    }
 }
