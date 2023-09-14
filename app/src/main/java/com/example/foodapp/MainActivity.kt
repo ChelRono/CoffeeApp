51926a5c3b33aa48b36bb18d8a2a05003f1c249c
Valarie <Valarierono97@gmail.com>
2023-09-14 12:38:22 +0300

    Added descriptive text to the background

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,6 +19,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.foodapp.ui.theme.FoodAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,8 +51,33 @@ fun MainContent() {
             contentDescription ="",
             contentScale = ContentScale.FillWidth,
             modifier = Modifier
-
         )
+        MainContentInfo()
     }
 }
 
+@Composable
+fun MainContentInfo(){
+   Column(
+       modifier = Modifier
+           .padding(horizontal = 24.dp)
+   ) {
+       Text(
+           text="Get Started",
+           fontSize = 20.sp,
+           color=Color.Yellow
+       )
+       Spacer(modifier = Modifier.height(10.dp))
+       Text(
+           text="Find the best coffee",
+           fontSize = 26.sp,
+           color=Color.White
+       )
+       Spacer(modifier = Modifier.height(10.dp))
+       Text(
+           text="for you",
+           fontSize = 26.sp,
+           color=Color.White
+       )
+   }
+}
