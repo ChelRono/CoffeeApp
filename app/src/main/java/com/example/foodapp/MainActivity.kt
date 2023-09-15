625702f53b2508d8f2083f74700ed0fad5bc5e1d
Valarie <Valarierono97@gmail.com>
2023-09-15 19:58:49 +0300

    Sign up page

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.foodapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,17 +11,28 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,6 +43,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.foodapp.ui.theme.FoodAppTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,24 +62,38 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainContent() {
-    Column(
-        modifier = Modifier
-            .background(Color.Black)
 
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.img),
-            contentDescription ="",
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier
-                .height(520.dp)
-        )
-        MainContentInfo()
-    }
+    val pagerState = rememberPagerState()
+    HorizontalPager(state = pagerState,pageCount = 3, pageSize = PageSize.Fill) { page ->
+        when(page) {
+
+            0 -> {
+
+                Column(
+                    modifier = Modifier
+                        .background(Color.Black)
+
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.img),
+                        contentDescription = "",
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier
+                            .height(550.dp)
+                    )
+                    MainContentInfo()
+
+                }
+            }
+            1 -> {
+                PagerTwo()
+            }
+        }}
 }
-
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainContentInfo(){
    Column(
@@ -75,7 +102,7 @@ fun MainContentInfo(){
    ) {
        Text(
            text="Get Started",
-           fontSize = 20.sp,
+           fontSize = 18.sp,
            color=Color( 0xFFffa500)
        )
        Spacer(modifier = Modifier.height(10.dp))
@@ -113,7 +140,7 @@ fun MainContentInfo(){
               .padding(start=100.dp)
       ){
           Button(
-              onClick = {  },
+              onClick = {},
               shape=RoundedCornerShape(17.dp),
               colors = ButtonDefaults.buttonColors(Color(0xFFffa500)),
               modifier = Modifier
@@ -132,3 +159,27 @@ fun MainContentInfo(){
 
    }
 }
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun PagerTwo(){
+    var text by remember { mutableStateOf("") }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black),
+
+        ) {
+            Text(
+                text = "Sign Up",
+                color=Color(0xFFffa500),
+                fontSize = 23.sp,
+                modifier = Modifier
+                    .padding(vertical = 140.dp, horizontal = 24.dp)
+            )
+            TextField(
+                value = text, onValueChange = { text = it },
+            )
+        }
+    }
