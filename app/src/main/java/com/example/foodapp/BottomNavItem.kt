077dc446973fab83bf5eb0e7ae838914e28a9ddf
Valarie <Valarierono97@gmail.com>
2023-09-16 18:23:58 +0300

    Created a class for the nav items

---
+++ app/src/main/java/com/example/foodapp/BottomNavItem.kt
@@ -0,0 +1,8 @@
+package com.example.foodapp
+
+sealed class BottomNavItem(var title:String, var icon:Int, var screen_route:String){
+
+    object Home : BottomNavItem("Home", R.drawable.home,"home")
+    object Cart: BottomNavItem("Cart",R.drawable.shopping_bag,"cart")
+    object Settings: BottomNavItem("Settings",R.drawable.settings,"settings")
+}
--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -21,11 +21,19 @@ import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -38,6 +46,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -105,7 +114,7 @@ fun MainContentInfo(){
            .padding(horizontal = 38.dp, vertical = 20.dp)
    ) {
        Text(
-           text="Get Started",
+           text="Coffee&Cookies",
            fontSize = 18.sp,
            color=Color( 0xFFffa500)
        )
@@ -122,39 +131,24 @@ fun MainContentInfo(){
            color=Color.White
        )
        Spacer(modifier = Modifier.height(10.dp))
-       Row {
-           Button(
-               onClick = {  },
-               shape=RoundedCornerShape(10.dp),
-               colors = ButtonDefaults.buttonColors(Color.Black),
-               modifier = Modifier
 
-
-           ) {
-               Text(
-                   text ="Skip Now",
-                   fontSize = 14.sp,
-                   color= Color.White
-
-               )
-           }
       Row (
           horizontalArrangement = Arrangement.End,
           modifier = Modifier
-              .padding(start=100.dp)
+              .padding(start=180.dp)
       ){
           Button(
               onClick = {},
               shape=RoundedCornerShape(17.dp),
               colors = ButtonDefaults.buttonColors(Color(0xFFffa500)),
               modifier = Modifier
-                  .width(110.dp)
+                  .width(140.dp)
                   .height(50.dp)
 
 
           ) {
               Text(
-                  text ="Next",
+                  text ="Get Started",
 
                   )
           }
@@ -162,7 +156,7 @@ fun MainContentInfo(){
        }
 
    }
-}
+
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -284,3 +278,4 @@ fun BottomText(){
        }
     }
 }
+
